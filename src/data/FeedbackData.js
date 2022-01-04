52eb5d9972792334bf2ee4980a50265294dd971b
AJOsmaston <dajoxx@gmail.com>
2022-01-04 16:23:10 +0000

    example feedback data

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Honestly, pretty good."
+  },
+  {
+    id: 2,
+    rating: 1,
+    text: "I'm not sure why, but this sucked. So much."
+  },
+  {
+    id: 3,
+    rating: 5,
+    text: "I think I fell asleep. Not sure."
+  }
+]
+
+export default FeedbackData;
