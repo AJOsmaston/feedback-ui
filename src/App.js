5b47042315b7aa8fb44ce23c0fe8b4ba070203ae
AJOsmaston <dajoxx@gmail.com>
2022-01-10 16:28:24 +0000

    moved feedback to Context

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import AboutIconLink from './components/AboutIconLink';
 
@@ -25,26 +26,27 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className='container'>
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackForm handleAdd={addFeedback}/>
-              <FeedbackStats feedback={feedback} />
-              <FeedbackList 
-                feedback={feedback}
-                handleDelete={deleteFeedback}
-              />
-            </>
-          }>  
-          </Route>
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink />
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats />
+                <FeedbackList 
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }>  
+            </Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
