75656f2a584024f8f419e5334c4dc793cc0638b7
AJOsmaston <dajoxx@gmail.com>
2022-01-12 16:41:53 +0000

    fetch added to json server mock db

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {v4 as uuidv4} from 'uuid'
-import FeedbackData from "../data/FeedbackData";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({children}) => {
 
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
-  })
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, [])
+
+  //fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   //to delete feedback
   const deleteFeedback = (id) => {
@@ -43,6 +56,7 @@ export const FeedbackProvider = ({children}) => {
   return <FeedbackContext.Provider value={{
     feedback, 
     feedbackEdit,
+    isLoading,
     deleteFeedback, 
     addFeedback, 
     editFeedback,
